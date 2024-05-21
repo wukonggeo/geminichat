e3d6b14f61af88cc3a537ee362fb1a074da0668f
wukonggeo <140490295+wukonggeo@users.noreply.github.com>
2024-05-21 10:21:20 +0800

    Gemino_Pro_Vision.py
    Update model version

--- pages/2_Gemino_Pro_Vision.py
+++ pages/2_Gemino_Pro_Vision.py
@@ -22,7 +22,7 @@ if "app_key" not in st.session_state:
 
 try:
     genai.configure(api_key = st.session_state.app_key)
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-pro-vision-latest	')
 except AttributeError as e:
     st.warning("Please Put Your Gemini App Key First.")
 
