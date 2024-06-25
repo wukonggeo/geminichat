ae01c00cee05b8628c5e329bc12e386e429ceda0
wukonggeo <140490295+wukonggeo@users.noreply.github.com>
2024-06-25 15:53:48 +0800

    1_Gemini_Pro.py
    Update

--- 1_Gemini_Pro.py
+++ 1_Gemini_Pro.py
@@ -45,7 +45,8 @@ else:
     st.caption("a chatbot, powered by google gemini pro.")
     
     if "app_key" not in st.session_state:
-        app_key = st.text_input("Your Gemini App Key", type='password')
+        # app_key = st.text_input("Your Gemini App Key", type='password')
+        app_key = st.secrets["Your Gemini App Key"]
         if app_key:
             st.session_state.app_key = app_key
     
