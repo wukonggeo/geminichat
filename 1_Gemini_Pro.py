0c16de852ac309d3d86f11884f6f591d61d02c38
wukonggeo <140490295+wukonggeo@users.noreply.github.com>
2025-02-10 18:38:32 +0800

    Update 1_Gemini_Pro.py

--- 1_Gemini_Pro.py
+++ 1_Gemini_Pro.py
@@ -57,8 +57,8 @@ else:
         genai.configure(api_key = st.session_state.app_key)
     except AttributeError as e:
         st.warning("Please Put Your Gemini App Key First.")
-    # gemini-2.0-pro-exp-0205 gemini-2.0-flash-thinking-exp-0121
-    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    # gemini-1.5-pro-latest gemini-2.0-flash-thinking-exp-0121 
+    model = genai.GenerativeModel('gemini-2.0-pro-exp-02-05')
     chat = model.start_chat(history = st.session_state.history)
     
     with st.sidebar:
