08a744f0af0cc373ab9fc5248c9fcdb3dd2474c7
wukonggeo <140490295+wukonggeo@users.noreply.github.com>
2025-02-25 10:17:21 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 
-# BLOCK_ONLY_HIGH
 """https://ai.google.dev/api/rest/v1beta/HarmCategory"""
 
 SAFETY_SETTTINGS = [
@@ -9,22 +8,22 @@ SAFETY_SETTTINGS = [
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_ONLY_HIGH",
     },
     {
         "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_ONLY_HIGH",
     },
     {
         "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_ONLY_HIGH",
     },
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_ONLY_HIGH",
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
+        "threshold": "BLOCK_ONLY_HIGH",
     },
 ]
