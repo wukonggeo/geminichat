29203a85d67f7e5702f8cd9a0805c0547906a997
wukonggeo <140490295+wukonggeo@users.noreply.github.com>
2025-06-10 14:32:06 +0800

    Update 2_Gemini_Think.py

--- pages/2_Gemini_Think.py
+++ pages/2_Gemini_Think.py
@@ -17,8 +17,8 @@ model_options = {
     'gemini-2.0-pro-exp-02-05': "Pro",
     'gemini-2.0-flash': "Flash",
     'gemini-2.0-flash-exp': "Vison",
-    'gemini-2.0-flash-thinking-exp-01-21': "Think-Flash",
-    'gemini-2.5-flash-preview-04-17': "Think-Flash-new",
+    'gemini-2.5-flash-preview-04-17': "Think-Flash",
+    'gemini-2.5-flash-preview-05-20': "Think-Flash-new",
     "gemini-2.5-pro-exp-03-25":"Think-PRO"
     }
 default_index = list(model_options.keys()).index('gemini-2.0-flash')
