d0d1f0fe97b9666e21a3b5e6ddf2722e8f349356
wukonggeo <140490295+wukonggeo@users.noreply.github.com>
2025-08-20 14:39:00 +0800

    Update 2_Gemini_Think.py

--- pages/2_Gemini_Think.py
+++ pages/2_Gemini_Think.py
@@ -88,51 +88,12 @@ def show_message(prompt, loading_str, image=None):
         st.session_state.history_pic = model_chat.history
 
 
-def save_uploaded_pdf(uploaded_file, save_path):
-  """
-  保存上传的 PDF 文件到本地。
-  """
-  try:
-    with open(save_path, "wb") as f:  # 以二进制写入模式打开文件
-      f.write(uploaded_file.getbuffer())  # 将上传文件的内容写入文件
-    return True
-  except Exception as e:
-    print(f"保存文件失败: {e}")  # 打印错误信息，方便调试
-    return False
-
-
-def clear_other_pdfs(dir_path, keep_filename=None):
-    """删除指定目录下除 keep_filename 外的所有pdf文件。"""
-    pdf_files = dir_path.glob("*.pdf")
-    for f in pdf_files:
-        if keep_filename is not None and f.name == keep_filename:
-            continue
-        try:
-            f.unlink()
-        except Exception as e:
-            st.warning(f"删除 {f} 失败: {e}")
-
-@st.cache_data(show_spinner=False)
-def input_file(file):
-    # uploaded_file = st.file_uploader('请打开一个文件', type=['pdf'], accept_multiple_files=True)
-    file_save_path = None
-    if file:
-        clear_other_pdfs(BASE_PATH, keep_filename=file.name)
-        file_save_path = BASE_PATH / file.name
-        save_uploaded_pdf(file, file_save_path)
-    with st.spinner("正在处理文件..."):
-        time.sleep(2)
-    return file_save_path
-
-
 image = None
-file_path = None
 if "app_key" in st.session_state and st.session_state.app_key is not None:
     uploaded_file = st.file_uploader("choose a pic or pdf...", type=["pdf", "jpg", "png", "jpeg", "gif"], label_visibility='collapsed', on_change = clear_state)
     if uploaded_file is not None:
         if uploaded_file.type == "application/pdf":
-            file_path = input_file(uploaded_pdf)
-            # client.files.upload(file="invoice.pdf", config={'display_name': 'invoice'})
+            st.info("正在处理PDF文件")
         else:
             image = Image.open(uploaded_image).convert('RGB')
             image_bytes = image.tobytes()
